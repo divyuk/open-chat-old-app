4c1f947150bea7dbbbf45cd10023c6652c05414d
divyuk <div14kes@gmail.com>
2023-11-05 14:54:26 +0530

    Comments on Error Middleware

--- src/middlewares/error.middlewares.js
+++ src/middlewares/error.middlewares.js
@@ -1,5 +1,17 @@
 import mongoose from "mongoose";
 import { ApiError } from "../utilis/ApiError";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+/**
+ *
+ * @param {Error | ApiError} err
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ * @param {import("express").NextFunction} next
+ *
+ *
+ * @description This middleware is responsible to catch the errors from any request handler wrapped inside the {@link asyncHandler}
+ */
 
 const errorHandler = (err, req, res, next) => {
   let error = err;
