86fa27312561f3e363d80eeffdb99353998d4c38
divyuk <div14kes@gmail.com>
2023-11-05 14:53:08 +0530

    Comments for ApiError Class

--- src/utilis/ApiError.js
+++ src/utilis/ApiError.js
@@ -1,4 +1,16 @@
+import { errorHandler } from "../middlewares/error.middlewares.js";
+/**
+ * @description Common Error class to throw an error from anywhere.
+ * The {@link errorHandler} middleware will catch this error at the central place and it will return an appropriate response to the client
+ */
 class ApiError extends Error {
+  /**
+   *
+   * @param {number} statusCode
+   * @param {string} message
+   * @param {any[]} errors
+   * @param {string} stack
+   */
   constructor(
     statusCode,
     message = "Something went wrong",
