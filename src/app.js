c6731ac6e25527e0fdcef39470486c842b7e98dc
divyuk <div14kes@gmail.com>
2023-11-05 13:22:12 +0530

    Socket Test Connection

--- src/app.js
+++ src/app.js
@@ -6,4 +6,15 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {});
 
+app.get("/", (req, res) => {
+  res.json("Homepage");
+});
+
+io.on("connection", (socket) => {
+  console.log("A user is connected.!");
+});
+
+// App routes
+app.use("/api/v1/users", userRouter);
+
 export { httpServer };
