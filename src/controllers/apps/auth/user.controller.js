9b8683af52e57bf39d077aaf42f8c955981a33b9
divyuk <div14kes@gmail.com>
2023-11-05 13:30:41 +0530

    Get the user body

--- src/controllers/apps/auth/user.controller.js
+++ src/controllers/apps/auth/user.controller.js
@@ -1,3 +1,5 @@
 import { asyncHandler } from "../../../utilis/asyncHandler";
 
-const registerUser = asyncHandler(req, res);
+const registerUser = asyncHandler((req, res) => {
+  const { email, username, password, role } = req.body;
+});
