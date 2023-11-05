41499c2d95219d8ea38e57dd2aa64f61b0dd7d8a
divyuk <div14kes@gmail.com>
2023-11-05 14:38:39 +0530

    Find existing user

--- src/controllers/apps/auth/user.controller.js
+++ src/controllers/apps/auth/user.controller.js
@@ -1,5 +1,11 @@
 import { asyncHandler } from "../../../utilis/asyncHandler";
 
-const registerUser = asyncHandler((req, res) => {
+const registerUser = asyncHandler(async (req, res) => {
   const { email, username, password, role } = req.body;
+  const existedUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (existedUser) {
+  }
 });
