4533288e066270193b19700363d85e8e5528374d
divyuk <div14kes@gmail.com>
2023-11-05 14:52:56 +0530

    Error Middleware

---
+++ src/middlewares/error.middlewares.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { ApiError } from "../utilis/ApiError";
+
+const errorHandler = (err, req, res, next) => {
+  let error = err;
+
+  // Check if the error is instance of an ApiError class which extends native Error Class
+  if (!(error instanceof ApiError)) {
+    //if not then create a new ApiError instance to keep the consistency
+
+    // assign an appropriate status code
+    const statusCode =
+      error.statusCode || error instanceof mongoose.Error ? 400 : 500;
+    // set a message from native Error instance or a custom one
+    const message = error.message || "Something went wrong";
+    error = new ApiError(statusCode, message, error?.errors || [], err.stack);
+  }
+
+  //Now we are sure that the `error` variable will be an instance of ApiError Class
+  const response = {
+    ...error,
+    message: error.message,
+    ...(process.env.NODE_ENV === "development" ? { stack: error.stack } : {}),
+  };
+
+  return res.status(error.statusCode).json(response);
+};
+
+export { errorHandler };
