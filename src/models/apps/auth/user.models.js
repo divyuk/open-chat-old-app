a3dbb3464c00c48aa6f6f8615335f65a117f25a5
divyuk <div14kes@gmail.com>
2023-11-05 15:47:19 +0530

    Updated Method for user schema

--- src/models/apps/auth/user.models.js
+++ src/models/apps/auth/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import crypto from "crypto";
+import { USER_TOKEN_EXPIRY } from "../../../../constants";
 const userSchema = new Schema(
   {
     avatar: {
@@ -37,10 +38,23 @@ const userSchema = new Schema(
       refreshToken: {
         type: String,
       },
+      emailVerificationToken: {
+        type: String,
+      },
+      emailVerificationExpiry: {
+        type: Date,
+      },
     },
   },
   { timestamps: true }
 );
 //!The timestamps option is one of these schema-level options, and it is used to automatically add "createdAt" and "updatedAt" fields to your documents. These fields will be automatically updated by Mongoose whenever a document is created or updated. It's clear that these options apply globally to the entire document, and Mongoose can interpret and process them correctly.
+
+userSchema.methods.generateToken = function () {
+  const hashedToken = crypto.createHash("sha256").digest("hex");
+  const tokenExpiry = Date.now() + USER_TOKEN_EXPIRY;
+
+  return { hashedToken, tokenExpiry };
+};
 
 export const User = mongoose.model("User", userSchema);
